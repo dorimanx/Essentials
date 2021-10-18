aeefe58071bbd47b45f54e962f91b8383a0118b4
Yuri <Yuri@bynet.co.il>
2021-10-18 15:17:33 +0300

    EntityModule, cleanups and fixes.

--- Essentials/Commands/EntityModule.cs
+++ Essentials/Commands/EntityModule.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +6,6 @@ using System.Text;
 using Sandbox.Engine.Multiplayer;
 using Sandbox.Game.Entities;
 using Sandbox.Common.ObjectBuilders;
-using Sandbox.Game.World;
 using Sandbox.ModAPI;
 using Torch.Commands;
 using Torch.Commands.Permissions;
@@ -26,20 +25,17 @@ namespace Essentials
     [Category("entities")]
     public class EntityModule : CommandModule
     {
-#pragma warning disable 649
         [ReflectedGetter(Name = "m_clientStates")]
-        private static Func<MyReplicationServer, IDictionary> _clientStates;
+        public static Func<MyReplicationServer, IDictionary> _clientStates;
 
-        private const string CLIENT_DATA_TYPE_NAME = "VRage.Network.MyClient, VRage";
-        [ReflectedGetter(TypeName = CLIENT_DATA_TYPE_NAME, Name = "Replicables")]
-        private static Func<object, MyConcurrentDictionary<IMyReplicable, MyReplicableClientData>> _replicables;
+        [ReflectedGetter(TypeName = "VRage.Network.MyClient, VRage", Name = "Replicables")]
+        public static Func<object, MyConcurrentDictionary<IMyReplicable, MyReplicableClientData>> _replicables;
 
-        [ReflectedMethod(Name = "RemoveForClient", OverrideTypeNames = new[] { null, CLIENT_DATA_TYPE_NAME, null })]
-        private static Action<MyReplicationServer, IMyReplicable, object, bool> _removeForClient;
+        [ReflectedMethod(Name = "RemoveForClient", OverrideTypeNames = new string[] { null, "VRage.Network.MyClient, VRage", null })]
+        public static Action<MyReplicationServer, IMyReplicable, object, bool> _removeForClient;
 
         [ReflectedMethod(Name = "ForceReplicable")]
-        private static Action<MyReplicationServer, IMyReplicable, Endpoint> _forceReplicable;
-#pragma warning restore 649
+        public static Action<MyReplicationServer, IMyReplicable, Endpoint> _forceReplicable;
 
         private static Dictionary<ulong, DateTime> _commandtimeout = new Dictionary<ulong, DateTime>();
 
@@ -51,50 +47,47 @@ namespace Essentials
                 return;
 
             var steamid = Context.Player.SteamUserId;
+            if (steamid < 1)
+                return;
+
             if (_commandtimeout.TryGetValue(steamid, out DateTime lastcommand))
             {
                 TimeSpan difference = DateTime.Now - lastcommand;
-                if (difference.TotalMinutes < 1)
+                if (difference.TotalMinutes < 1.0)
                 {
                     Context.Respond($"Cooldown active. You can use this command again in {difference.TotalSeconds:N0} seconds");
                     return;
                 }
-                else
-                {
-                    _commandtimeout[steamid] = DateTime.Now;
-                }
+                _commandtimeout[steamid] = DateTime.Now;
             }
             else
-            {
                 _commandtimeout.Add(steamid, DateTime.Now);
-            }
 
-            var playerEndpoint = new Endpoint(Context.Player.SteamUserId, 0);
-            var replicationServer = (MyReplicationServer)MyMultiplayer.ReplicationLayer;
-            var clientDataDict = _clientStates.Invoke(replicationServer);
-            object clientData;
             try
             {
-                clientData = clientDataDict[playerEndpoint];
+                var playerEndpoint = new Endpoint(Context.Player.SteamUserId, 0);
+                var replicationServer = (MyReplicationServer)MyMultiplayer.ReplicationLayer;
+                var clientDataDict = _clientStates.Invoke(replicationServer);
+                object clientData = clientDataDict[playerEndpoint];
+
+                var clientReplicables = _replicables.Invoke(clientData);
+                var replicableList = new List<IMyReplicable>(clientReplicables.Count);
+
+                replicableList.AddRange(from pair in clientReplicables
+                                        select pair.Key);
+
+                foreach (var replicable in replicableList)
+                {
+                    _removeForClient.Invoke(replicationServer, replicable, clientData, true);
+                    _forceReplicable.Invoke(replicationServer, replicable, playerEndpoint);
+                }
+                Context.Respond($"Forced replication of {replicableList.Count} entities.");
             }
             catch
             {
+                // Avoid Client crash if some entitie is null.
                 return;
             }
-
-            var clientReplicables = _replicables.Invoke(clientData);
-
-            var replicableList = new List<IMyReplicable>(clientReplicables.Count);
-            foreach (var pair in clientReplicables)
-                replicableList.Add(pair.Key);
-
-            foreach (var replicable in replicableList)
-            {
-                _removeForClient.Invoke(replicationServer, replicable, clientData, true);
-                _forceReplicable.Invoke(replicationServer, replicable, playerEndpoint);
-            }
-
-            Context.Respond($"Forced replication of {replicableList.Count} entities.");
         }
 
         [Command("stop", "Stops an entity from moving")]
@@ -144,7 +137,7 @@ namespace Essentials
              * and can also kill the player while being seated. 
              */
             var player = Utilities.GetPlayerByNameOrId(playerName);
-            if (player != null) 
+            if (player != null)
             {
                 MyVisualScriptLogicProvider.SetPlayersHealth(player.IdentityId, 0);
 
@@ -158,12 +151,13 @@ namespace Essentials
              * If we could not find the player there is a chance he is offline, in that case we try inflicting
              * damage to the character as the VST will not help us with offline characters. 
              */
-            if (!Utilities.TryGetEntityByNameOrId(playerName, out IMyEntity entity)) {
+            if (!Utilities.TryGetEntityByNameOrId(playerName, out IMyEntity entity))
+            {
                 Context.Respond($"Entity '{playerName}' not found.");
                 return;
             }
 
-            if (entity is IMyCharacter) 
+            if (entity is IMyCharacter)
             {
                 var destroyable = entity as IMyDestroyableObject;
 
@@ -198,7 +192,6 @@ namespace Essentials
         [Permission(MyPromoteLevel.SpaceMaster)]
         public void PowerOff(string name)
         {
-
             if (string.IsNullOrEmpty(name))
                 return;
 
@@ -213,6 +206,7 @@ namespace Essentials
                 Context.Respond("Command do not work on characters.");
                 return;
             }
+
             IMyCubeGrid grid = entity as MyCubeGrid;
             var blocks = new List<IMySlimBlock>();
             grid.GetBlocks(blocks, f => f.FatBlock != null && f.FatBlock is IMyFunctionalBlock
@@ -225,14 +219,12 @@ namespace Essentials
                 item.Enabled = false;
             }
             Context.Respond($"Entity '{entity.DisplayName}' powered off");
-
         }
 
         [Command("poweron", "Power on entities with the given text in their name.")]
         [Permission(MyPromoteLevel.SpaceMaster)]
         public void PowerOn(string name)
         {
-
             if (string.IsNullOrEmpty(name))
                 return;
 
@@ -247,6 +239,7 @@ namespace Essentials
                 Context.Respond("Command do not work on characters.");
                 return;
             }
+
             IMyCubeGrid grid = entity as MyCubeGrid;
             var blocks = new List<IMySlimBlock>();
             grid.GetBlocks(blocks, f => f.FatBlock != null && f.FatBlock is IMyFunctionalBlock
@@ -254,73 +247,65 @@ namespace Essentials
             f.FatBlock.BlockDefinition.TypeId == typeof(MyObjectBuilder_BatteryBlock) ||
             f.FatBlock.BlockDefinition.TypeId == typeof(MyObjectBuilder_SolarPanel)));
             var list = blocks.Select(f => (IMyFunctionalBlock)f.FatBlock).Where(f => !f.Enabled).ToArray();
+
             foreach (var item in list)
             {
                 item.Enabled = true;
             }
             Context.Respond($"Entity '{entity.DisplayName}' powered on");
-
         }
 
         [Command("eject", "Ejects a specific player from any block they are seated in, or all players in the server if run with 'all'")]
-        public void Eject(string playerName) {
-
-            if (playerName.ToLower() == "all") 
-            {
+        public void Eject(string playerName)
+        {
+            if (playerName.ToLower() == "all")
                 EjectAllPlayers();
-            }
-            else 
-            {
+            else
                 EjectSinglePlayer(playerName);
-            }
         }
 
-        private void EjectAllPlayers() {
-
+        private void EjectAllPlayers()
+        {
             int ejectedPlayersCount = 0;
 
-            foreach (var grid in MyEntities.GetEntities().OfType<MyCubeGrid>().ToList()) 
+            foreach (var grid in MyEntities.GetEntities().OfType<MyCubeGrid>().ToList())
             {
-                foreach (var controller in grid.GetFatBlocks<MyShipController>()) 
+                foreach (var controller in grid.GetFatBlocks<MyShipController>())
                 {
-                    if (controller.Pilot != null) 
+                    if (controller.Pilot != null)
                     {
                         controller.Use();
                         ejectedPlayersCount++;
                     }
                 }
             }
-
             Context.Respond($"Ejected '{ejectedPlayersCount}' players from their seats.");
         }
 
-        private void EjectSinglePlayer(string playerName) {
-
+        private void EjectSinglePlayer(string playerName)
+        {
             /* We check first if the player is among the online players before looping over all grids for nothing. */
             var player = Utilities.GetPlayerByNameOrId(playerName);
-            if (player != null) 
+            if (player != null)
             {
                 /* If he is online we check if he is currently seated. If he is eject him. */
-                if (player?.Controller.ControlledEntity is MyCockpit controller) 
+                if (player?.Controller.ControlledEntity is MyCockpit controller)
                 {
                     controller.Use();
                     Context.Respond($"Player '{playerName}' ejected.");
-                } 
-                else 
-                {
-                    Context.Respond("Player not seated.");
                 }
+                else
+                    Context.Respond("Player not seated.");
 
                 return;
             }
 
-            foreach (var grid in MyEntities.GetEntities().OfType<MyCubeGrid>().ToList()) 
+            foreach (var grid in MyEntities.GetEntities().OfType<MyCubeGrid>().ToList())
             {
-                foreach (var controller in grid.GetFatBlocks<MyShipController>()) 
+                foreach (var controller in grid.GetFatBlocks<MyShipController>())
                 {
-                    var pilot = controller.Pilot;
 
-                    if (pilot != null && pilot.DisplayName == playerName) 
+                    if ((controller?.Pilot) != null && (controller?.Pilot).DisplayName == playerName)
                     {
                         controller.Use();
 
